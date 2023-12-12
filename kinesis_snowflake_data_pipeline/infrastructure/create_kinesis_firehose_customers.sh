945671b1e3555f1c45cc768282bd2b64202b58d8
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-12-12 15:57:13 -0600

    Update create_kinesis_firehose_customers.sh

--- kinesis_snowflake_data_pipeline/infrastructure/create_kinesis_firehose_customers.sh
+++ kinesis_snowflake_data_pipeline/infrastructure/create_kinesis_firehose_customers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read configuration from JSON file
-config_file="kinesis_firehose_config.json"
+config_file="kinesis_firehose_delivery.json"
 
 # Create Kinesis Firehose Delivery Stream
 aws firehose create-delivery-stream --cli-input-json file://$config_file
