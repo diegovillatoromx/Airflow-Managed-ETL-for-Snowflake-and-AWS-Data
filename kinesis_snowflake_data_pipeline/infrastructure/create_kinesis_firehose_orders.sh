f96a1585b6c6fee13ab4ed25725ba195c55d9701
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-12-12 16:00:05 -0600

    Update and rename create_kinesis_firehose_customers.sh to create_kinesis_firehose_orders.sh

--- kinesis_snowflake_data_pipeline/infrastructure/create_kinesis_firehose_orders.sh
+++ kinesis_snowflake_data_pipeline/infrastructure/create_kinesis_firehose_orders.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read configuration from JSON file
-config_file="kinesis_firehose_customers.json"
+config_file="kinesis_firehose_orders.json"
 
 # Create Kinesis Firehose Delivery Stream
 aws firehose create-delivery-stream --cli-input-json file://$config_file
