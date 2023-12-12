4dcf2ae519a912c2e463a378a40b36a3656c20e6
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-12-12 14:57:28 -0600

    Update create_iam_role.sh

--- kinesis_snowflake_data_pipeline/infrastructure/create_iam_role.sh
+++ kinesis_snowflake_data_pipeline/infrastructure/create_iam_role.sh
@@ -4,6 +4,7 @@
 IAM_ROLE_NAME=$(jq -r .iam_role_name config/iam_role_config.json)
 IAM_ROLE_DESCRIPTION=$(jq -r .iam_role_description config/iam_role_config.json)
 
+
 # Crear el rol IAM
 IAM_ROLE_ARN=$(aws iam create-role --role-name $IAM_ROLE_NAME --assume-role-policy-document file://path/to/assume_role_policy.json --description "$IAM_ROLE_DESCRIPTION" --output text --query 'Role.Arn')
 
