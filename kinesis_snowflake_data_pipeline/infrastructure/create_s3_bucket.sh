74d4f0b61018fb08703869b66ea47e2bb40bdad2
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-12-17 07:21:17 -0600

    Update create_s3_bucket.sh

--- kinesis_snowflake_data_pipeline/infrastructure/create_s3_bucket.sh
+++ kinesis_snowflake_data_pipeline/infrastructure/create_s3_bucket.sh
@@ -3,7 +3,7 @@
 # Configuración
 S3_BUCKET_NAME=$(jq -r .s3_bucket_name config/s3_bucket_config.json)
 AWS_REGION=$(jq -r .aws_region config/s3_bucket_config.json)
-
+ 
 # Crear el bucket de S3
 aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $AWS_REGION
 
