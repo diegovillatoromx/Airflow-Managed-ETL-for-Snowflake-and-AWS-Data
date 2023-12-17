40802cf29f81fc309144eb146f7afef47f2ef8c0
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-12-17 07:21:47 -0600

    Update create_s3_bucket.sh

--- kinesis_snowflake_data_pipeline/infrastructure/create_s3_bucket.sh
+++ kinesis_snowflake_data_pipeline/infrastructure/create_s3_bucket.sh
@@ -4,7 +4,7 @@
 S3_BUCKET_NAME=$(jq -r .s3_bucket_name config/s3_bucket_config.json)
 AWS_REGION=$(jq -r .aws_region config/s3_bucket_config.json)
  
-# Crear el bucket de S3
+# Crear el bucket de S3 
 aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $AWS_REGION
 
 # Configurar el bucket para el acceso público
